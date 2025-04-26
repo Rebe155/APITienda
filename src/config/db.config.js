82a2fa4d6dcced3635fdffcc6effade0b1e5a92a
Rebe155 <arevalorebeca41@gmail.com>
2025-04-26 14:36:59 -0600

    Tienda API

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,23 +1,11 @@
-require('dotenv').config(); // Cargar las variables de entorno desde el archivo .
+require('dotenv').config();
 
-let dbConfig;
-
-if (process.env.NODE_ENV == 'production') {
-    dbConfig = {
+const dbConfig = {
     HOST: process.env.DB_HOST,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB_NAME,
-    PORT: process.env.DB_PORT,
-    };
-} else {
-    dbConfig = {
-    HOST: process.env.DB_HOST || "localhost",
-    USER: process.env.DB_USER || "root",
-    PASSWORD: process.env.DB_PASSWORD || "",
-    DB: process.env.DB_NAME || "tienda",
-    PORT: process.env.DB_PORT || 3306,
-    };
-}
+    DB: process.env.DB_DATABASE,
+    PORT: process.env.DB_PORT
+};
 
 module.exports = dbConfig;
--- src/models/db.js
+++ src/models/db.js
@@ -1,13 +1,21 @@
 const mysql = require('mysql2');
-const dbConfig = require('../config/db.config');
+const dbConfig = require('../config/db.config.js');
 
-// Crear la conexión a la base de datos
 const connection = mysql.createConnection({
     host: dbConfig.HOST,
     user: dbConfig.USER,
     password: dbConfig.PASSWORD,
     database: dbConfig.DB,
     port: dbConfig.PORT
+});
+
+connection.connect(error => {
+    if (error) {
+        console.error('❌ Error de conexión a la base de datos:', error.message);
+        process.exit(1); // Se detiene el servidor si falla
+    } else {
+        console.log('✅ Conexión exitosa a la base de datos.');
+    }
 });
 
 module.exports = connection;
